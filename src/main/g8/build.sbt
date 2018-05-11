da229dd657d31d896cc0904d2e6ac64fec33e49a
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-05-11 17:08:10 +0200

    improve doc/ formatting and trigger test compilation in 'paradox' task

--- src/main/g8/build.sbt
+++ src/main/g8/build.sbt
@@ -1,30 +1,32 @@
 lazy val root = (project in file("."))
   .enablePlugins(GitBranchPrompt, GitVersioning, ParadoxPlugin)
+  // all these settings are only relevant to the "root" project which is why they are not defined in CommonSettingsPlugin.scala
   .settings(
-    // all these settings are only relevant to the "root" project which is why they are not defined in CommonSettingsPlugin.scala
     name := "$name$-root",
-
-    paradoxProperties ++= Map(
-      "version" -> version.value
-    ),
-
+    // trigger dump-license-report in all other projects and rename the output
+    // (paradox uses and first heading as link name in '@@@index' containers AND cannot handle variables in links)
     (mappings in Compile) in paradoxMarkdownToHtml ++= Seq(
       dumpLicenseReport.value / (licenseReportTitle.value + ".md") -> "licenses/root.md",
       (core / dumpLicenseReport).value / ((core / licenseReportTitle).value + ".md") -> "licenses/core.md"
     ),
-
+    // trigger test compilation in projects which contain snippets to be shown in the documentation
+    paradox in Compile := {
+      val _ = (core / compile in Test).value
+      (paradox in Compile).value
+    },
+    // properties to be accessible from within the documentation
+    paradoxProperties ++= Map(
+      "version" -> version.value
+    ),
     paradoxTheme := Some(builtinParadoxTheme("generic")),
-
+    // this project is not supposed to be used externally, so don't publish
     publish := {}
-
   )
 
 lazy val core = (project in file("core"))
   .enablePlugins(GitBranchPrompt, GitVersioning)
   .settings(
     name := "$name$",
-
     resolvers ++= Dependencies.resolvers,
-
     libraryDependencies ++= Dependencies.coreLibraries
   )
--- src/main/g8/project/CommonSettingsPlugin.scala
+++ src/main/g8/project/CommonSettingsPlugin.scala
@@ -1,5 +1,8 @@
 import com.typesafe.sbt.GitPlugin.autoImport.git
-import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.{HeaderLicense, headerLicense}
+import de.heikoseeberger.sbtheader.HeaderPlugin.autoImport.{
+  HeaderLicense,
+  headerLicense
+}
 import sbt._
 import sbt.Keys._
 
@@ -7,22 +10,22 @@ object CommonSettingsPlugin extends CommonSettingsPluginTpl {
 
   // the rationale for placing settings defs here is that they should (or can) not be updated automatically using the scala-base-sync script
   // in the following, organizationName and startYear would also be required by sbt-header to generate ready-made license headers
-  override lazy val projectSettings: Seq[Def.Setting[_]] = tplProjectSettingsPlus(
-    git.baseVersion := "0.0.0",
-
-    // TODO: see https://github.com/sbt/sbt-header#configuration for setting up a license header
-    headerLicense := Some(HeaderLicense.Custom(
-      s"""TODO: define a license header
-         |""".stripMargin
-    )),
-
-    // TODO: (e.g.: "MIT" -> "https://opensource.org/licenses/MIT")
-    licenses += "TODO: license type" -> url("https://TODO/uri/of/project/license"),
-
-    organization := "$organization$",
-
-    organizationName := "$organization_name$",
-
-    startYear := Some($start_year$)
-  )
+  override lazy val projectSettings: Seq[Def.Setting[_]] = {
+    tplProjectSettingsPlus(
+      git.baseVersion := "0.0.0",
+      // TODO: see https://github.com/sbt/sbt-header#configuration for setting up a license header
+      headerLicense := Some(
+        HeaderLicense.Custom(
+          s"""TODO: define a license header
+             |""".stripMargin
+        )),
+      // TODO: (e.g.: "MIT" -> "https://opensource.org/licenses/MIT")
+      licenses += "TODO: license type" -> url(
+        "https://TODO/uri/of/project/license"
+      ),
+      organization := "$organization$",
+      organizationName := "$organization_name$",
+      startYear := Some($start_year$)
+    )
+  }
 }
