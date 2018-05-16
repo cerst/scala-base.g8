eb36f23314a3e25f9e2aa68ab290a73443959541
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-05-16 15:28:41 +0200

    fix implicit collision of '%' helper method

--- src/main/g8/project/ModuleIDSyntax.scala
+++ src/main/g8/project/ModuleIDSyntax.scala
@@ -2,8 +2,11 @@ import sbt._
 
 final class ModuleIDSyntax(moduleID: ModuleID) {
 
-  def %(first: Configuration, additional: Configuration*): ModuleID = {
-    val configurations = (first +: additional).mkString(",")
+  def %(first: Configuration,
+        second: Configuration,
+        additional: Configuration*): ModuleID = {
+
+    val configurations = (first +: second +: additional).mkString(",")
     moduleID.withConfigurations(Some(configurations))
   }
 
