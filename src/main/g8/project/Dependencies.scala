a460d8d8cd779602b9b21694e5bd3c4323ada548
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-05-16 11:52:52 +0200

    add inline comment concerning inline documentation of library licenses

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -9,6 +9,8 @@ object Dependencies {
 
   }
 
+  // comment licenses for dependencies using the SPDX short identifier (see e.g. https://opensource.org/licenses/Apache-2.0)
+  // rationale: double check the license when adding a new library avoid having to remove a problematic one later on when it is in use and thus hard to remove
   object Library {
 
   }
