5d538c18ee5d63f426fbc881cd7b551ef17ea127
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-07-19 09:17:19 +0200

    fix incorrect git tag parsing and add missing included-file(s)

--- src/main/g8/doc/src/main/paradox/changelog/index.md
+++ src/main/g8/doc/src/main/paradox/changelog/index.md
@@ -1,2 +1,5 @@
 # Changelog
 
+## 0.1.2
+* Fix _scripts/scala-base-sync/run.sh_ not parsing the latest tag correctly
+* Add missing _project/project/sbt-updates.sbt_ to _scripts/scala-base-sync/included-files_
--- src/main/g8/scripts/scala-base-sync/run.sh
+++ src/main/g8/scripts/scala-base-sync/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-ORIGIN_TAG="v0.1.1"
+ORIGIN_TAG="v0.1.2"
 REPO_URL="https://github.com/cerst/scala-base.g8"
 SCRIPT_FOLDER_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
@@ -22,7 +22,7 @@ rm -rf ${TMP_DIR}
 git clone ${REPO_URL} ${TMP_DIR}
 
 echo "Checking script tags"
-LATEST_TAG=$( cd ${TMP_DIR} && git describe --tags)
+LATEST_TAG=$( cd ${TMP_DIR} && git describe --abbrev=0 --tags)
 if [ "$LATEST_TAG" != "$ORIGIN_TAG" ]; then
     echo "Origin tag for this script ($ORIGIN_TAG) differs from latest one ($LATEST_TAG)."
     echo "Please update the scala-base-sync folder manually and retry (don't forget to remove unwanted files again)"
