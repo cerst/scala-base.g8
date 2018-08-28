673973be7ce0bd7e2257adb8bf0024d024efafa4
Constantin Gerstberger <constantin.gerstberger@xaasify.com>
2018-08-28 14:41:03 +0200

    add SbtNativePackagerSettingsPluginTpl to 'verbatim' rather than escaping '$' within as this file is copied when using the sync script

--- src/main/g8/project/SbtNativePackagerSettingsPluginTpl.scala
+++ src/main/g8/project/SbtNativePackagerSettingsPluginTpl.scala
@@ -14,12 +14,12 @@ object SbtNativePackagerSettingsPluginTpl extends AutoPlugin {
     // * application.conf
     // * log4j2.xml
     // --
-    // Note that during packaging, 'src/universal/**' is copied to '\${app_home}/../**'
-    // e.g.: 'src/universal/conf' -> '\${app_home}/../conf'
+    // Note that during packaging, 'src/universal/**' is copied to '${app_home}/../**'
+    // e.g.: 'src/universal/conf' -> '${app_home}/../conf'
     // i.e. you can package production specific config (and more) that way
     bashScriptExtraDefines ++= Seq(
-      """addJava "-Dconfig.file=\${app_home}/../conf/application.conf"""",
-      """addJava "-Dlog4j.configurationFile=\${app_home}/../conf/log4j2.xml""""
+      """addJava "-Dconfig.file=${app_home}/../conf/application.conf"""",
+      """addJava "-Dlog4j.configurationFile=${app_home}/../conf/log4j2.xml""""
     )
   }
 
